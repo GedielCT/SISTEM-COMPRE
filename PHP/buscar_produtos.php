dbebbf6860b97cc4534074c161fec47f0173b713
GedielCT <gedielcassemiro@gmail.com>
2025-09-09 11:34:57 -0300

    Testando banco de dados

---
+++ PHP/buscar_produtos.php
@@ -0,0 +1,25 @@
+<?php
+$hostname = "localhost";
+$bancodedados = "sistemcompre";
+$usuario = "root";
+$senha = "";
+
+$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+if ($mysqli->connect_errno) {
+    http_response_code(500);
+    echo json_encode(["erro" => "Falha ao conectar"]);
+    exit;
+}
+
+$sql = "SELECT codigo, quantidade, valor_unitario, nome_produto, marca_produto, categoria FROM cadastro_produto";
+$result = $mysqli->query($sql);
+
+$produtos = [];
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        $produtos[] = $row;
+    }
+}
+header('Content-Type: application/json');
+echo json_encode($produtos);
+?>
--- conexao.php
+++ conexao.php
@@ -9,7 +9,18 @@ $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
 if ($mysqli->connect_errno) {
     echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-else
-    echo "Conectado ao Banco de dados"
+else {
+    // Exemplo de consulta
+    $sql = "SELECT * FROM cadastro_produto";
+    $result = $mysqli->query($sql);
+
+    if ($result) {
+        while ($row = $result->fetch_assoc()) {
+            echo "Codigo: " . $row['codigo'] . " - Quantidade: " . $row['quantidade'] . "<br>";
+        }
+    } else {
+        echo "Erro na consulta: " . $mysqli->error;
+    }
+}   
 
 ?>
